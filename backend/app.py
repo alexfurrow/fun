cc37ce4b92d14b5d8ea596e14576308a460a17ee
alex <alexanderfurrow@gmail.com>
2024-12-10 19:16:33 -0600

    basic frontend

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    # Running in debug mode for convenience
+    app.run(debug=True)
